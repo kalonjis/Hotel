29ef266946afada0e21b4d57c69e83abd66df319
Stephane Kalonji <kalonjistephane@gmail.com>
2024-08-19 16:39:01 +0200

    region Constructeurs init

--- Hotel.java
+++ Hotel.java
@@ -7,5 +7,19 @@ public class Hotel {
     private List <Reservation> listeReservations;
     // endregion
 
+    // region Constructeurs
+    public Hotel() {
+        listeChambres = new ArrayList<>();
+        listeReservations = new ArrayList<>();
+    }
 
+    public Hotel(List<Chambre> listeChambres) {
+        this.listeChambres = listeChambres;
+    }
+
+    public Hotel(List<Chambre> listeChambres, List<Reservation> listeReservations) {
+        this.listeChambres = listeChambres;
+        this.listeReservations = listeReservations;
+    }
+    //endregion
 }
